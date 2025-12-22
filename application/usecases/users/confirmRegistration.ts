ce3264181cd51bf3214e628508e8ee53acd6d816
Joshua Grandville <joshua77130@live.fr>
2025-12-22 10:22:25 +0100

    save

--- application/usecases/users/confirmRegistration.ts
+++ application/usecases/users/confirmRegistration.ts
@@ -1,32 +1,55 @@
-import { UserRepository } from "@application/repositories/users"
-import { EmailConfirmationTokenRepository } from "@application/repositories/emailConfirmationTokens"
-import { NotFoundError, ValidationError } from "@application/errors"
-import { AccountRepository } from "@application/repositories/account"
-import { IBANGenerator } from "@application/services/IBANGenreator"
+import { UserRepository } from "@application/repositories/users";
+import { EmailConfirmationTokenRepository } from "@application/repositories/emailConfirmationTokens";
+import { NotFoundError, ValidationError } from "@application/errors";
+import { AccountRepository } from "@application/repositories/account";
+import { IBANGenerator } from "@application/services/IBANGenreator";
+import { UuidGenerator } from "@application/services/UuidGenerator";
+import { Account } from "@domain/entities/account";
+import { AccountType } from "@domain/values/accountType";
+import { StatusAccount } from "@domain/values/statusAccount";
 
 export class ConfirmRegistration {
-    constructor(
-        private readonly userRepository: UserRepository,
-        private readonly emailConfirmationTokenRepository: EmailConfirmationTokenRepository,
-        private readonly accountRepository: AccountRepository,
-        private readonly ibanGenerator: IBANGenerator,
-    ) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly emailConfirmationTokenRepository: EmailConfirmationTokenRepository,
+    private readonly accountRepository: AccountRepository,
+    private readonly ibanGenerator: IBANGenerator,
+    private readonly uuidGenerator: UuidGenerator
+  ) {}
 
-    async execute(token: string): Promise<void> {
-        const confirmationToken = await this.emailConfirmationTokenRepository.findByToken(token)
-        
-        if (!confirmationToken) {
-            throw new NotFoundError("Invalid or expired confirmation token")
-        }
+  async execute(token: string): Promise<void> {
+    const confirmationToken =
+      await this.emailConfirmationTokenRepository.findByToken(token);
 
-        if (confirmationToken.isExpired()) {
-            await this.emailConfirmationTokenRepository.deleteByToken(token)
-            throw new ValidationError("Confirmation token has expired")
-        }
-
-        await this.userRepository.setEmailVerified(confirmationToken.userId, new Date())
-        await this.emailConfirmationTokenRepository.deleteByToken(token)
-        const IBAN = this.ibanGenerator.generate();
-        await this.accountRepository.createAccountForUser(confirmationToken.userId, IBAN);
+    if (!confirmationToken) {
+      throw new NotFoundError("Invalid or expired confirmation token");
     }
+
+    if (confirmationToken.isExpired()) {
+      await this.emailConfirmationTokenRepository.deleteByToken(token);
+      throw new ValidationError("Confirmation token has expired");
+    }
+
+    await this.userRepository.setEmailVerified(
+      confirmationToken.userId,
+      new Date()
+    );
+    await this.emailConfirmationTokenRepository.deleteByToken(token);
+
+    const accountId = this.uuidGenerator.generate();
+    const iban = this.ibanGenerator.generate();
+    const account = new Account(
+      accountId,
+      AccountType.CURRENT,
+      iban,
+      "Main Account",
+      false,
+      0,
+      0,
+      StatusAccount.OPEN,
+      confirmationToken.userId,
+      0
+    );
+    await this.accountRepository.save(account);
+  }
 }
--- infrastructure/express/controllers/UserController.ts
+++ infrastructure/express/controllers/UserController.ts
@@ -1,31 +1,31 @@
-import { RegisterUser } from "@application/usecases/registerUser"
-import { GetAllUsers } from "@application/usecases/getAllUsers"
-import { LoginUser } from "@application/usecases/loginUser"
-import { ConfirmRegistration } from "@application/usecases/confirmRegistration"
-import { RegisterUserInput, LoginUserInput } from "@application/requests/auth"
-import { User } from "@domain/entities/users"
+import { RegisterUser } from "@application/usecases/users/registerUser";
+import { GetAllUsers } from "@application/usecases/users/getAllUsers";
+import { LoginUser } from "@application/usecases/users/loginUser";
+import { ConfirmRegistration } from "@application/usecases/users/confirmRegistration";
+import { RegisterUserInput, LoginUserInput } from "@application/requests/auth";
+import { User } from "@domain/entities/users";
 
 export class UserController {
-    public constructor(
-        private readonly registerUser: RegisterUser,
-        private readonly loginUser: LoginUser,
-        private readonly getAllUsers: GetAllUsers,
-        private readonly confirmRegistration: ConfirmRegistration,
-    ) {}
+  public constructor(
+    private readonly registerUser: RegisterUser,
+    private readonly loginUser: LoginUser,
+    private readonly getAllUsers: GetAllUsers,
+    private readonly confirmRegistration: ConfirmRegistration
+  ) {}
 
-    public async register(payload: RegisterUserInput): Promise<void> {
-        await this.registerUser.execute(payload)
-    }
+  public async register(payload: RegisterUserInput): Promise<void> {
+    await this.registerUser.execute(payload);
+  }
 
-    public async confirmRegistrationToken(token: string): Promise<void> {
-        await this.confirmRegistration.execute(token)
-    }
+  public async confirmRegistrationToken(token: string): Promise<void> {
+    await this.confirmRegistration.execute(token);
+  }
 
-    public async login(payload: LoginUserInput): Promise<User> {
-        return this.loginUser.execute(payload)
-    }
+  public async login(payload: LoginUserInput): Promise<User> {
+    return this.loginUser.execute(payload);
+  }
 
-    public async list(): Promise<User[]> {
-        return this.getAllUsers.execute()
-    }
+  public async list(): Promise<User[]> {
+    return this.getAllUsers.execute();
+  }
 }
